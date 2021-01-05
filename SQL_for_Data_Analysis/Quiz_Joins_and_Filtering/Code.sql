eac1519e1441c6239bf67d2efd83b0979ed636b2
DAVID MERCADO FAUSTINO <davidmercadofaustino@gmail.com>
2021-01-05 11:39:27 -0300

    Update Code.sql

--- SQL_for_Data_Analysis/Quiz_Joins_and_Filtering/Code.sql
+++ SQL_for_Data_Analysis/Quiz_Joins_and_Filtering/Code.sql
@@ -35,3 +35,17 @@ ON accounts.sales_rep_id = sales_reps.id
 WHERE region.name ='Midwest'
 AND sales_reps.name LIKE '% K%'
 ORDER BY accounts.name;
+
+
+/*
+4. Exercicio
+*/
+SELECT region.name region, accounts.name account, orders.total_amt_usd/(orders.total + 0.01) unit_price
+FROM sales_reps
+JOIN region
+ON sales_reps.region_id = region.id
+JOIN accounts
+ON accounts.sales_rep_id = sales_reps.id
+JOIN orders
+ON orders.account_id = accounts.id
+WHERE orders.standard_qty > 100;
