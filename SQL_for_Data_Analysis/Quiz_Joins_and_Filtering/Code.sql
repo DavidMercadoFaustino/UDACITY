6e64037ecb2ff2eae5e57049a475338a0b73a84b
DAVID MERCADO FAUSTINO <davidmercadofaustino@gmail.com>
2021-01-05 11:43:39 -0300

    Update Code.sql

--- SQL_for_Data_Analysis/Quiz_Joins_and_Filtering/Code.sql
+++ SQL_for_Data_Analysis/Quiz_Joins_and_Filtering/Code.sql
@@ -49,3 +49,19 @@ ON accounts.sales_rep_id = sales_reps.id
 JOIN orders
 ON orders.account_id = accounts.id
 WHERE orders.standard_qty > 100;
+
+
+/*
+5. Exercicio
+*/
+SELECT region.name region, accounts.name account, orders.total_amt_usd/(orders.total + 0.01) unit_price
+FROM sales_reps
+JOIN region
+ON sales_reps.region_id = region.id
+JOIN accounts
+ON accounts.sales_rep_id = sales_reps.id
+JOIN orders
+ON orders.account_id = accounts.id
+WHERE orders.standard_qty > 100
+AND orders.poster_qty > 50
+ORDER BY unit_price ASC;
