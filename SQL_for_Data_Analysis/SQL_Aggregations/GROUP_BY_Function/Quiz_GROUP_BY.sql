3e97076595c48f2016228e44bcf3be6f03c39342
DAVID MERCADO FAUSTINO <davidmercadofaustino@gmail.com>
2021-01-06 12:45:54 -0300

    Update Quiz_GROUP_BY.sql

--- SQL_for_Data_Analysis/SQL_Aggregations/GROUP_BY_Function/Quiz_GROUP_BY.sql
+++ SQL_for_Data_Analysis/SQL_Aggregations/GROUP_BY_Function/Quiz_GROUP_BY.sql
@@ -1,14 +1,24 @@
 /*1. Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.*/
+SELECT accounts.name, MIN(orders.occurred_at) AS earliest_order
+FROM accounts
+JOIN orders
+ON orders.account_id = accounts.id
+GROUP BY accounts.name
+ORDER BY earliest_order;
+
+/* 2. Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.*/
 
 
-Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+/*3. Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.*/
 
-Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
 
-Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+/*4. Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.*/
 
-Who was the primary contact associated with the earliest web_event?
 
-What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+/*5. Who was the primary contact associated with the earliest web_event?*/
 
-Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
+
+/*6. What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.*/
+
+
+/*7. Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.*/
